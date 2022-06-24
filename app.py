90fb238501a0d5a179b3084a02aa833c6409076c
Marcos Vinicius <marcosvinicius@MacBook-Pro-de-Marcos.local>
2022-06-24 20:55:27 -0300

    v1.2.0

--- app.py
+++ app.py
@@ -10,26 +10,23 @@ from datetime import datetime
 
 import os
 
-version = os.environ.get('MODEL_VERSION')
+model_version = os.environ.get('MODEL_VERSION')
+server_version = os.environ.get('SERVER_VERSION')
 
 def load_model(version):
     url = "https://raw.github.com/Marcos30004347/tweet_prediction_models/main/" + version
     date = datetime.now().timestamp()
     directory = getcwd()
-    
     filename = directory + "/" + version
-    
     req = requests.get(url)
-    
     open(filename,'wb').write(req.content)
-    
     return pickle.load(open(filename, 'rb'))
 
-model_data = load_model(version)
+model_data = load_model(model_version)
 
 model = model_data['model']
 
-date = datetime.utcfromtimestamp(int(model_data['model_date'])).strftime('%Y-%m-%d %H:%M:%S')
+model_date = datetime.utcfromtimestamp(int(model_data['model_date'])).strftime('%Y-%m-%d %H:%M:%S')
 
 app = Flask(__name__)
 
@@ -41,7 +38,7 @@ def api_american():
   
     return jsonify({
         "is_american": str(is_american[0]),
-        "version": str(version),
-        "model_date": str(date)
+        "version": str(server_version),
+        "model_date": str(model_date)
     })
   
